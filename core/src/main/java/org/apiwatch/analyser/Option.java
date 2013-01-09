81e5032436d8d877968739c0f896fef160fdfbdc
robin-jarry <robin.jarry@gmail.com>
2013-01-09 01:33:59 +0100

    added missing license headers

--- core/src/main/java/org/apiwatch/analyser/Option.java
+++ core/src/main/java/org/apiwatch/analyser/Option.java
@@ -1,3 +1,10 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2012, Robin Jarry, ABlogiX. All rights reserved.      *
+ *                                                                     *
+ * This file is part of APIWATCH and published under the BSD license.  *
+ *                                                                     *
+ * See the "LICENSE" file for more information.                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package org.apiwatch.analyser;
 
 public class Option {
--- core/src/main/java/org/apiwatch/util/antlr/IterableTreeAdaptor.java
+++ core/src/main/java/org/apiwatch/util/antlr/IterableTreeAdaptor.java
@@ -1,3 +1,10 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2012, Robin Jarry, ABlogiX. All rights reserved.      *
+ *                                                                     *
+ * This file is part of APIWATCH and published under the BSD license.  *
+ *                                                                     *
+ * See the "LICENSE" file for more information.                        *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package org.apiwatch.util.antlr;
 
 import org.antlr.runtime.RecognitionException;
